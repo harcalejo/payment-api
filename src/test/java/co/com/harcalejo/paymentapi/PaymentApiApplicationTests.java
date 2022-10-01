0fb4d72dc90fa47440a96a3cc8759ccf67c13de0
Harcalejo <harcalejo@gmail.com>
2022-09-30 21:17:14 -0500

    Initial commit

---
+++ src/test/java/co/com/harcalejo/paymentapi/PaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.harcalejo.paymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
